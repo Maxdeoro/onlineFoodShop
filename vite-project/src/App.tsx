526498689dc4a1ee07779a9fd30957b054048f89
Maxdeoro <maxdeoro@gmail.com>
2025-06-30 22:31:33 +0300

    types for hooks and events

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,21 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import Button, { ButtonAlt } from './components/button/Button';
+import { useState, type MouseEvent } from 'react';
 import './App.css';
 import { ButtonAlt } from './components/button/Button';
 
 
 function App() {
 
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (e: MouseEvent) => {                 // MouseEvent from react!
+    console.log(e);
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
   return (
     <>
-      <ButtonAlt onClick={() => console.log('clicked alt')}>ButtonAlt</ButtonAlt>
+      <ButtonAlt onClick={addCounter}>ButtonAlt</ButtonAlt>
     </>
   )
 }
