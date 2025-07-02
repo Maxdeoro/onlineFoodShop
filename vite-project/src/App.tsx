19140a05cbb9161fd129a99212a39c5d8922d27a
Maxdeoro <maxdeoro@gmail.com>
2025-07-02 22:18:19 +0300

    create Routes as JSX component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,10 @@ import { useState, type MouseEvent } from 'react';
 // import './App.css';
 import { Button } from './components/button/Button';
 import Input from './components/input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/menu/Menu';
+import { Cart } from './pages/cart/Cart';
+import { Error } from './pages/error/Error';
 
 
 function App() {
@@ -19,6 +23,15 @@ function App() {
       <Button appearance='small' onClick={addCounter}>Button</Button>
       <Button appearance='big' onClick={addCounter}>Button</Button>
       <Input placeholder='input here'/>
+      <div>
+        <a href='/'>Menu</a><br/>
+        <a href='/cart'>Cart</a>
+      </div>
+      <Routes>
+        <Route path='/' element={<Menu/>}/>
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   )
 }
