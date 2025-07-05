2c9ece77f85e82f5e6a25e5e4f753a8106058ae9
Maxdeoro <maxdeoro@gmail.com>
2025-07-05 23:42:58 +0300

    layout sidebar Menu

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+// import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Menu } from './pages/menu/Menu.tsx';
 import { Cart } from './pages/cart/Cart.tsx';
 import { Error } from './pages/error/Error.tsx';
-import { Layout } from './layout/menu/Menu.tsx';
+import { Layout } from './layout/menu/Layout.tsx';
 
 const myRouter = createBrowserRouter([
   {
@@ -31,7 +31,7 @@ const myRouter = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     <RouterProvider router={myRouter}></RouterProvider>
   </StrictMode>
 )
