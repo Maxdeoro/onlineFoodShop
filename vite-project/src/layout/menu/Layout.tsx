117c780e6068ca5c3afd1c2881dc776ec0a2929e
Maxdeoro <maxdeoro@gmail.com>
2025-07-06 20:45:42 +0300

    menu with useLocation

--- vite-project/src/layout/menu/Layout.tsx
+++ vite-project/src/layout/menu/Layout.tsx
@@ -1,8 +1,17 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styles from './Layout.module.css';
 import { Button } from "../../components/button/Button";
+import { useEffect } from "react";
+import cn from 'classnames';
 
 export function Layout() {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log(location);
+    }, [location]);
+
     return <div className={styles['layout']}>
                 <div className={styles['sidebar']}>
                     <div className={styles['user']}>
@@ -11,11 +20,15 @@ export function Layout() {
                         <div className={styles['email']}>jsnow@yahoo.com</div>
                     </div>
                     <div className={styles['menu']}>
-                        <Link to='/' className={styles['link']}>
+                        <Link to='/' className={cn(styles['link'], {
+                            [styles.active]: location.pathname === '/',
+                        })}>
                             <img src="/menu-icon.svg" alt="menu icon"/>
                             Menu
                         </Link>
-                        <Link to='/cart' className={styles['link']}>
+                        <Link to='/cart' className={cn(styles['link'], {
+                            [styles.active]: location.pathname === '/cart',
+                        })}>
                             <img src='cart-icon.svg' alt='cart icon'/>
                             Cart
                         </Link>
