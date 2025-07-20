3b664df0c1bc6e634d3727de75a8600da8183b7f
Maxdeoro <maxdeoro@gmail.com>
2025-07-20 22:18:07 +0300

    Login page tsx & css

--- vite-project/src/pages/login/Login.tsx
+++ vite-project/src/pages/login/Login.tsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/button/Button';
+import Header from '../../components/header/Header';
+import Input from '../../components/input/Input';
 import styles from './Login.module.css';
+import type { FormEvent } from 'react';
+
+const submit = (e: FormEvent) => {
+    e.preventDefault();             // won't reload page
+    console.log(e);
+};
 
 function Login() {
-    return <></>;
+    return <div className={styles['loginWrapper']} onSubmit={submit}>
+                <Header>Login</Header>
+                <form className={styles['loginForm']}>
+                    <div className={styles['field']}>
+                        <label htmlFor='email'>Your email</label>
+                        <Input id='email' placeholder='Email'/>
+                    </div>
+                    <div className={styles['field']}>
+                        <label htmlFor='password'>Your password</label>
+                        <Input id='password' type='password' placeholder='Password'/>
+                    </div>
+                    <Button appearance='big'>Login</Button>
+                </form>
+                <div className={styles['links']}>
+                    <div>Don't you have an account?</div>
+                    <Link to='/auth/registration'>Register.</Link>
+                </div>
+           </div>;
 };
 
 export default Login;
