26c12f1de325bac91c90aa76782f9570aaa9aa4b
Maxdeoro <maxdeoro@gmail.com>
2025-07-30 23:15:33 +0300

    requests in actions & errors handling

--- vite-project/src/pages/login/Login.tsx
+++ vite-project/src/pages/login/Login.tsx
@@ -3,13 +3,10 @@ import { Button } from '../../components/button/Button';
 import Header from '../../components/header/Header';
 import Input from '../../components/input/Input';
 import styles from './Login.module.css';
-import { useState, type FormEvent } from 'react';
-import axios, { AxiosError } from 'axios';
-import { PREFIX } from '../../helpers/API';
-import type { LoginResponse } from '../../interfaces/auth.interface';
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../../store/store';
-import { userActions } from '../../store/user.slice';
+import { useEffect, type FormEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootStore } from '../../store/store';
+import { login, userActions } from '../../store/user.slice';
 
 export type LoginForm = {
     email: {
@@ -24,13 +21,19 @@ export type LoginForm = {
 
 function Login() {
 
-    const [error, setError] = useState<string | undefined>();
     const navigate = useNavigate();
     const dispatch = useDispatch<AppDispatch>();
+    const {jwt, loginErrorMessage} = useSelector((state: RootStore) => state.user);
+
+    useEffect(() => {
+        if(jwt) {
+            navigate('/');
+        }
+    }, [jwt, navigate]);
 
     const submit = async (e: FormEvent) => {
     e.preventDefault();             // won't reload page
-    setError(undefined);
+    dispatch(userActions.clearLoginError());
     const target = e.target as typeof e.target & LoginForm;
     const {email, password} = target;
     console.log(email.value);
@@ -39,22 +42,12 @@ function Login() {
 };
 
     const sendLogin = async (email: string, password: string) => {
-    try {
-        const {data} = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {email, password});
-        console.log(data);
-        dispatch(userActions.addJwt(data.access_token));
-        navigate('/');
-    } catch(e) {
-        if(e instanceof AxiosError) {
-            console.log(e);
-            setError(e.response?.data.message);
-        }
-    }
-};
+        dispatch(login({email, password}));
+    };
 
     return <div className={styles['loginWrapper']} >
                 <Header>Login</Header>
-                {error && <div className={styles['error']}>{error}</div>}
+                {loginErrorMessage && <div className={styles['error']}>{loginErrorMessage}</div>}
                 <form className={styles['loginForm']} onSubmit={submit}>
                     <div className={styles['field']}>
                         <label htmlFor='email'>Your email</label>
